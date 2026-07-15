0067399206b578e7269740ff345699daf59acc6a
Atharva Borkar <217187637+atharva295-git@users.noreply.github.com>
2026-07-15 21:20:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 python -m src.preprocess
-python -m src.model
+python -m src.model  
 python -m src.fairness
 python -m src.explainer
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
