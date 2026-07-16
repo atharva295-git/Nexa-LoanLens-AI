4dbbb9ac1ddb8f371d48bd4e2ac746e4386152c1
Atharva Borkar <217187637+atharva295-git@users.noreply.github.com>
2026-07-16 12:51:51 +0530

    Fix port configuration

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 python -m src.preprocess
 python -m src.model
 python -m src.fairness
-streamlit run app.py --server.port $PORT --server.address 0.0.0.0 --server.headless true
+streamlit run app.py --server.port $PORT --server.address 0.0.0.0 --server.headless true --browser.gatherUsageStats false
